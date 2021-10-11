b92ae076aaa2af87bbf9688c79d1eee469b26fea
Selasi3 <selasiayittah3@gmail.com>
2021-10-10 15:46:26 -1200

    displays users input

--- custom_order.java
+++ custom_order.java
@@ -12,9 +12,12 @@ public class custom_order {
     // TEST CODE
     
     // STEP 2 CREATE A SCANNER OBEJCT, DECLARE VARAIBLES, & PRINT STATEMENTS
-	  
-
-	 
+	  Scanner keyboard = new Scanner(System.in);
+    String  firstName;
+    String itemOrder;
+    String frostingType;
+    String fillingType;
+    String toppings;
     // Introduce shop and prompt user to input first name
       
 
@@ -23,21 +26,34 @@ public class custom_order {
     // TEST CODE
     
     // STEP 3 INPUT YOUR NAME AND PREPARE TO VIEW MENU
-   
+    System.out.println("What is your first name?");
+firstName=keyboard.nextLine();
+System.out.print(firstName + ", please see our MENU below");
+System.out.print("\n");
 	  
       
     // TEST CODE     
     
     // STEP 4 DISPLAY MENU
-
-	
-      
+    System.out.println("_______________________________________________");      
+    System.out.println("        MENU         QUANTITY    BASE COST  ");
+    System.out.println("_______________________________________________"); 
+    System.out.println("        Cake                     1            $15     ");
+    System.out.println("   Set of Cupcakes       6            $15     ");
+    System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+    System.out.println("Frostings (vanilla, chocolate, strawberry, coco)");
+    System.out.println("Fillings (mocha, mint, lemon, caramel, vanilla)");
+    System.out.println("Toppings (sprinkles, cinnamon, cocoa, nuts)");
+    System.out.println("_______________________________________________");
+    System.out.println("Do you want CUPCAKES or a CAKE");
+    itemOrder = keyboard.nextLine()  ;
 
     // TEST CODE     
     
     // STEP 5 PROMPT USER TO ORDER
+    System.out.println("What type of FROSTING do you want? ");
+    System.out.println("Vanilla, Chocolate, Strawberry or Coco");
 
-  
 	  
 	  
 	  
@@ -45,16 +61,18 @@ public class custom_order {
     // TEST CODE
       
     // STEP 6 PROMPT USER TO CHOOSE FROSTING
-
+        frostingType=keyboard.nextLine();
    
-	 
+
 	 
 	 
       
     //TEST CODE
       
     // STEP 7 PROMPT USER TO CHOOSE FILLING
-
+        System.out.println("What type of FILLING do you want? ");
+        System.out.println("Mocha, Mint, Lemon, Caramel or Raspberry");
+        fillingType = keyboard.nextLine();
 
 	  
 	  
@@ -62,7 +80,9 @@ public class custom_order {
     // TEST CODE
       
     // STEP 8 PROMPT USER TO CHOOSE TOPPINGS
-
+        System.out.println("What type of TOPPINGS do you want? ");
+        System.out.println("Sprinkles, Cinnamon, Cocoa, Nuts");
+        toppings = keyboard.nextLine();
 
 	
       
@@ -70,6 +90,22 @@ public class custom_order {
     // TEST CODE
       
     // STEP 9 DISPLAY ORDER CONFIRMATION
+//        String  firstName;
+//        String itemOrder;
+//        String frostingType;
+//        String fillingType;
+//        String toppings;
+        System.out.println("_______________________________________________");
+String [] list = {itemOrder,frostingType,fillingType,toppings};
+        System.out.println("Your input are: ");
+        for (String item:
+             list) {
+            System.out.println(item);
+
+        }
+        System.out.println("_______________________________________________");
+
+//        System.out.println("firstName "+ firstName);
 
 
       
